40a2c2015dcab115fe6e2680d1b89aa3c3e736fe
Anjali <anjalisarin@gmail.com>
2020-06-16 00:01:55 +0100

    Added JS for sticky header and reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,19 @@
+//Imports
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
-let mobileMenu = new MobileMenu();
+//JS interactivity functions
+new MobileMenu();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
 
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60);
 
+new StickyHeader();
+
+//Webpack dev-server
 if (module.hot) {
     module.hot.accept();
 };
